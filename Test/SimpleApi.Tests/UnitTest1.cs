4030abb2f983d0376e9dce508a0aea60377cb749
madhuriparashar <madhuriparashar@gmail.com>
2022-02-06 23:19:00 +0530

    Phase 1

---
+++ Test/SimpleApi.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+using SimpleApi;
+
+
+namespace SimpleApi.Tests
+{
+    
+    public class UnitTest1
+    {
+        ValuesController valuesController = new ValuesController();
+        [Fact]
+        public void Test1()
+        {
+      var result =   valuesController.Get(1);
+        Assert.Equal("Unit Test Message",result);
+
+        }
+    }
+}
